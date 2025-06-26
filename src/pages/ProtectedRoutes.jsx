99780b0b18791f0117c263aa0d9fe14bd7f85899
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2025-06-26 15:13:43 +0530

    remove

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -2,23 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './login/loginPage';
 import RegisterPage from './register/registerPage';
 import HomePage from './dashbord/homePage';
-import PublicRoute from './PublicRoute';
 
 function ProtectedRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <PublicRoute>
-            <LoginPage />
-          </PublicRoute>
-        } />
-        <Route path="/register" element={
-          <PublicRoute>
-            <RegisterPage />
-          </PublicRoute>
-        } />
-
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
