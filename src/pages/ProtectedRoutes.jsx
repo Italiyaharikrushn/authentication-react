e6984c7a5d758b5bf456ce2f914a95027b000629
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2025-06-30 11:38:13 +0530

    other change

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -25,3 +25,36 @@ function ProtectedRoutes() {
 }
 
 export default ProtectedRoutes;
+
+
+
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import LoginPage from './login/loginPage';
+// import RegisterPage from './register/registerPage';
+// import HomePage from './dashbord/homePage';
+// import { useSelector } from 'react-redux';
+
+// function ProtectedRoutes() {
+//   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+//   return (
+//     <Router>
+//       <Routes>
+//         {!isAuthenticated && (
+//           <>
+//             <Route path="/login" element={<LoginPage />} />
+//             <Route path="/register" element={<RegisterPage />} />
+//             <Route path="*" element={<Navigate to="/login" />} />
+//           </>
+//         )}
+//         {isAuthenticated && (
+//           <>
+//             <Route path="/home" element={<HomePage />} />
+//             <Route path="*" element={<Navigate to="/home" />} />
+//           </>
+//         )}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default ProtectedRoutes;
